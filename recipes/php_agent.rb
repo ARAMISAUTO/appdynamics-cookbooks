9360b68ac7453cbfcb77522afe1d84ddd990080b
Tristan Rivoallan <tristan@rivoallan.net>
2017-09-22 17:15:57 +0200

    Revert "Reloading Apache does not work in this case"
    This reverts commit b11d06309417412f678bbb5f540a714a46a48039.

--- recipes/php_agent.rb
+++ recipes/php_agent.rb
@@ -119,13 +119,12 @@ when 'debian', 'ubuntu'
 
   # Enable appdynamics extension
   execute 'php5enmod appdynamics_agent' do
-    notifies :restart, 'service[apache2]' if resource_exists['service[apache2]']
+    notifies :reload, 'service[apache2]' if resource_exists['service[apache2]']
   end
 end
 
 # TODO : Create appdynamics-proxy service
 # TODO : Handle JVM configuration
-# TODO : User https://supermarket.chef.io/cookbooks/poise-service
 # https://github.com/ARAMISAUTO/com.aramisauto.platform/blob/master/provisioners/salt/srv/salt/base/appdynamics/php.sls
 # bash ./runProxy -d /opt/appdynamics/appdynamics-php-agent/proxy -r /opt/appdynamics/appdynamics-php-agent/proxy /tmp/proxy.communication /opt/appdynamics/appdynamics-php-agent/logs
 systemd_unit 'appdynamics-proxy.service' do
