84d7e29bc66a694958387adf514d2ae4fdc2f55e
Tristan Rivoallan <tristan@rivoallan.net>
2017-10-06 17:35:28 +0200

    [machine_agent] sysv => systemd

--- metadata.rb
+++ metadata.rb
@@ -8,13 +8,16 @@ long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url        'https://github.com/appdynamics/appdynamics-cookbooks'        if respond_to?(:source_url)
 issues_url        'https://github.com/appdynamics/appdynamics-cookbooks/issues' if respond_to?(:issues_url)
 
-depends 'windows', '~> 1.44.3'
-depends 'python', '~> 1.4.6'
+depends 'apache2', '~> 5.0.0'
+depends 'apt', '~> 6.1.4'
+depends 'ark', '~> 1.1.0'
+depends 'java', '~> 1.42.0'
 depends 'nodejs', '~> 2.4.4'
 depends 'java', '~> 1.42.0'
-depends 'apt', '~> 3.0.0'
 depends 'powershell', '~> 3.0.0'
-depends 'ark', '~> 1.1.0'
+depends 'poise-python', '~> 1.6.0'
+depends 'poise-service', '~> 1.5.2'
+depends 'windows', '~> 1.44.3'
 
 # Red Hat
 supports 'amazon'
